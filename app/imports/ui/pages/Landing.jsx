a8252e123798bd688d3df7dec7f91760dcd196c4
Joel Vanta <122860229+joelvanta@users.noreply.github.com>
2023-04-05 11:24:31 -1000

    Update1

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,7 +6,7 @@ const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
     <Row className="align-middle text-center">
       <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+        <Image roundedCircle src="/images/manoaseal_transparent.png" width="150px" />
       </Col>
 
       <Col xs={8} className="d-flex flex-column justify-content-center">
