8573d3b93cf352bd0886e5c1fb52d3ecff6cd4c9
Joel Vanta <122860229+joelvanta@users.noreply.github.com>
2023-04-06 11:25:56 -1000

    Update

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -22,6 +22,7 @@ const NavBar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
             {currentUser ? ([
+              <Nav.Link id="user-profile-nav" as={NavLink} to="/userprofile" key="userprofile">Profile</Nav.Link>,
               <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Add Stuff</Nav.Link>,
               <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">List Stuff</Nav.Link>,
             ]) : ''}
---
+++ app/imports/ui/pages/UserProfile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+
+/* A simple static component to render some text for the landing page. */
+const UserProfile = () => (
+  <Container id="landing-page" fluid className="py-3">
+    <Row className="align-middle text-center">
+      <Col xs={4}>
+        <Image roundedCircle src="/images/manoaseal_transparent.png" width="150px" />
+      </Col>
+
+      <Col xs={8} className="d-flex flex-column justify-content-center">
+        <h1>Aloha! Welcome to Manoa Trade Your Stuff</h1>
+        <p>This is the user profile page</p>
+      </Col>
+
+    </Row>
+  </Container>
+);
+
+export default UserProfile;
