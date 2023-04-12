9d269321dac534d3389baebe5b79d7c2100eb43b
Hok Wai Chan <hokwai@hawaii.edu>
2023-04-12 03:15:30 -1000

    Removed Note

--- app/imports/ui/components/Profile.jsx
+++ app/imports/ui/components/Profile.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Card, Image, ListGroup } from 'react-bootstrap';
-import Note from './Note';
-import AddNote from './AddNote';
+import { Card, Image } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const Profile = ({ profile, notes }) => (
+const Profile = ({ profile }) => (
   <Card className="h-100" style={{ width: '400px' }}>
     <Card.Header>
       <Image src={profile.image} width={300} />
       <Card.Title>{profile.firstName} {profile.lastName}</Card.Title>
+    </Card.Header>
+    <Card.Body>
       <Card.Subtitle> Student ID: {profile.studentId}</Card.Subtitle>
       <Card.Subtitle> Email: {profile.email}</Card.Subtitle>
       <Card.Subtitle> Phone: {profile.phone}</Card.Subtitle>
       <Card.Subtitle> Position: {profile.position}</Card.Subtitle>
-    </Card.Header>
-    <Card.Body>
-      <Card.Text> About {profile.description}</Card.Text>
-      <ListGroup variant="flush">
-        {notes.map((note) => <Note key={note._id} note={note} />)}
-      </ListGroup>
-      <AddNote owner={profile.owner} profileId={profile._id} />
+      <Card.Subtitle> About: {profile.description}</Card.Subtitle>
       <Link to={`/editProfile/${profile._id}`}>Edit</Link>
     </Card.Body>
   </Card>
@@ -41,13 +35,6 @@ Profile.propTypes = {
     owner: PropTypes.string,
     _id: PropTypes.string,
   }).isRequired,
-  notes: PropTypes.arrayOf(PropTypes.shape({
-    note: PropTypes.string,
-    profileId: PropTypes.string,
-    owner: PropTypes.string,
-    createdAt: PropTypes.instanceOf(Date),
-    _id: PropTypes.string,
-  })).isRequired,
 };
 
 export default Profile;
--- app/imports/ui/pages/ListProfiles.jsx
+++ app/imports/ui/pages/ListProfiles.jsx
@@ -5,26 +5,22 @@ import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
 import Profile from '../components/Profile';
 import { Profiles } from '../../api/profile/Profiles';
-import { Notes } from '../../api/note/Notes';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const ListProfiles = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, profiles, notes } = useTracker(() => {
+  const { ready, profiles } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
     const subscription = Meteor.subscribe(Profiles.userPublicationName);
-    const subscription2 = Meteor.subscribe(Notes.userPublicationName);
     // Determine if the subscription is ready
-    const rdy = subscription.ready() && subscription2.ready();
+    const rdy = subscription.ready();
     // Get the Profile documents
     const profileItems = Profiles.collection.find({}).fetch();
-    // Get the Note documents
-    const noteItems = Notes.collection.find({}).fetch();
+
     return {
       profiles: profileItems,
-      notes: noteItems,
       ready: rdy,
     };
   }, []);
@@ -37,7 +33,7 @@ const ListProfiles = () => {
             <h2>List Profiles</h2>
           </Col>
           <Row xs={1} md={2} lg={3} className="g-4">
-            {profiles.map((profile) => (<Col key={profile._id}><Profile profile={profile} notes={notes.filter(note => (note.profileId === profile._id))} /> </Col>))}
+            {profiles.map((profile) => (<Col key={profile._id}><Profile profile={profile} /> </Col>))}
           </Row>
         </Col>
       </Row>
