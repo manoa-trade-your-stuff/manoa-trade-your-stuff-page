93c3f8200a113c2b1fac6294d1b0b9688d021535
Hok Wai Chan <hokwai@hawaii.edu>
2023-04-12 23:40:34 -1000

    Changed Admin List Profile

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -33,7 +33,7 @@ const NavBar = () => {
               <Nav.Link id="list-stuff-nav" as={NavLink} to="/listItem" key="listItem">Items List</Nav.Link>,
             ]) : ''}
             {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">Admin</Nav.Link>
+              <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">List Profiles</Nav.Link>
             ) : ''}
           </Nav>
           <Nav className="justify-content-end">
--- app/imports/ui/components/ProfileAdmin.jsx
+++ app/imports/ui/components/ProfileAdmin.jsx
@@ -4,17 +4,17 @@ import { Card, Image } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const ProfileAdmin = ({ profile }) => (
-  <Card className="h-100">
+  <Card className="h-100 mx-auto" style={{ width: '300px', fontFamily: 'Trebuchet MS, sans-serif' }}>
     <Card.Header>
-      <Image src={profile.image} width={75} />
-      <Card.Title>{profile.firstName} {profile.lastName}</Card.Title>
-      <Card.Subtitle>{profile.studentId}</Card.Subtitle>
-      <Card.Subtitle>{profile.email}</Card.Subtitle>
-      <Card.Subtitle>{profile.phone}</Card.Subtitle>
-      <Card.Subtitle>{profile.position}</Card.Subtitle>
+      <Image src={profile.image} width={150} />
     </Card.Header>
     <Card.Body>
-      <Card.Text>{profile.description}</Card.Text>
+      <Card.Title className="mb-3" style={{ fontSize: '30px', fontWeight: 'bold' }}>{profile.firstName} {profile.lastName}</Card.Title>
+      <Card.Subtitle className="mb-3" style={{ fontSize: '15px', fontWeight: 'bold' }}>Student ID: {profile.studentId}</Card.Subtitle>
+      <Card.Subtitle className="mb-3" style={{ fontSize: '15px', fontWeight: 'bold' }}>Email: {profile.email}</Card.Subtitle>
+      <Card.Subtitle className="mb-3" style={{ fontSize: '15px', fontWeight: 'bold' }}>Phone: {profile.phone}</Card.Subtitle>
+      <Card.Subtitle className="mb-3" style={{ fontSize: '15px', fontWeight: 'bold' }}>Position: {profile.position}</Card.Subtitle>
+      <Card.Subtitle className="mb-3" style={{ fontSize: '15px', fontWeight: 'bold' }}>About: {profile.description}</Card.Subtitle>
       <footer className="blockquote-footer">{profile.owner}</footer>
     </Card.Body>
   </Card>
