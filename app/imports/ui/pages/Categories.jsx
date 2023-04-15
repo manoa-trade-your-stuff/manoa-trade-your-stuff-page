8f1cbca294bfdfb737fe59840a541b2ccef88f10
kairitanaka <km321@hawaii.edu>
2023-04-14 15:17:21 -1000

    category

--- app/imports/ui/pages/Categories.jsx
+++ app/imports/ui/pages/Categories.jsx
@@ -3,7 +3,7 @@ import { Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Categories = () => (
-  <Row className="justify-content-center pt-5" id="size">
+  <Row className="justify-content-center pt-5">
     <Col xs={5}>
       <Col className="text-center" id="color"><h2>Category</h2></Col>
       <Col className="d-flex justify-content-center p-5 col px-md-5">
--- app/imports/ui/pages/CategoriesBook.jsx
+++ app/imports/ui/pages/CategoriesBook.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const CategoriesBook = () => (
-  <Button id="buttonSpace"><a href="Categories" id="color">Home</a></Button>
+  <Row className="justify-content-center">
+    <Button id="homeButton"><a href="Categories" id="color">Home</a></Button>
+    <Col xs={5}>
+      <Col className="text-center pt-2" id="color"><h2>BOOKS</h2></Col>
+      Books
+    </Col>
+  </Row>
 );
 
 export default CategoriesBook;
